f4129bc703c2c685244566679f299f5ced915c66
Ard-Jan van Etten <ardjanve@gmail.com>
2021-06-04 21:13:43 +0200

    werls

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@ use serde::{Deserialize, Serialize};
 use std::fs::File;
 use std::io::prelude::*;
 
+const HOOGTEFACTOR: f32 = 1.0 / 10.0;
+
 #[derive(Serialize, Deserialize)]
 struct Punt {
     lat: f32,
@@ -26,16 +28,16 @@ fn main() -> std::io::Result<()> {
 
     println!("{}", ritjes[0].lijn[0].hoogte);
 
-    let camera = raylib::camera::Camera3D::perspective(
+    let mut camera = raylib::camera::Camera3D::perspective(
         raylib::core::math::Vector3 {
-            x: 0.0,
-            y: 0.0,
-            z: 0.0,
+            x: 4.0,
+            y: 50.0,
+            z: 720.0 * HOOGTEFACTOR,
         },
         raylib::core::math::Vector3 {
             x: 4.2,
             y: 51.5,
-            z: 720.0,
+            z: 720.0 * HOOGTEFACTOR,
         },
         raylib::core::math::Vector3 {
             x: 0.0,
@@ -49,12 +51,17 @@ fn main() -> std::io::Result<()> {
         .size(800, 800)
         .title("Dienstregeling")
         .build();
+
     rl.set_target_fps(60);
+    rl.set_camera_mode(camera, CameraMode::CAMERA_FREE);
+
     while !rl.window_should_close() {
         let mut d = rl.begin_drawing(&thread);
         d.clear_background(Color::BLACK);
         {
             let mut d3 = d.begin_mode3D(camera);
+
+            d3.update_camera(&mut camera);
 
             for rit in &ritjes {
                 let hoeveelheidpunten = rit.lijn.len();
@@ -65,13 +72,13 @@ fn main() -> std::io::Result<()> {
                     let punta = Vector3 {
                         x: fysiek_punt_a.lng,
                         y: fysiek_punt_a.lat,
-                        z: fysiek_punt_a.hoogte,
+                        z: fysiek_punt_a.hoogte * HOOGTEFACTOR,
                     };
 
                     let puntb = Vector3 {
                         x: fysiek_punt_b.lng,
                         y: fysiek_punt_b.lat,
-                        z: fysiek_punt_b.hoogte,
+                        z: fysiek_punt_b.hoogte * HOOGTEFACTOR,
                     };
 
                     d3.draw_line_3D(punta, puntb, Color::YELLOW);
