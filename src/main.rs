eecee71f2d2a8b824195b0189a72e5c4765ea2f6
Ard-Jan van Etten <ardjanve@gmail.com>
2021-06-04 20:41:48 +0200

    werks

--- src/main.rs
+++ src/main.rs
@@ -33,45 +33,53 @@ fn main() -> std::io::Result<()> {
             z: 0.0,
         },
         raylib::core::math::Vector3 {
-            x: 1.0,
-            y: 1.0,
-            z: 1.0,
+            x: 4.2,
+            y: 51.5,
+            z: 720.0,
         },
         raylib::core::math::Vector3 {
             x: 0.0,
-            y: 1.0,
-            z: 0.0,
+            y: 0.0,
+            z: 1.0,
         },
         45.0,
     );
 
     let (mut rl, thread) = raylib::init()
-        .size(640, 480)
+        .size(800, 800)
         .title("Dienstregeling")
         .build();
-    
     rl.set_target_fps(60);
-    
     while !rl.window_should_close() {
         let mut d = rl.begin_drawing(&thread);
-        
         d.clear_background(Color::BLACK);
+        {
+            let mut d3 = d.begin_mode3D(camera);
 
-        let mut d3 = d.begin_mode3D(camera);
+            for rit in &ritjes {
+                let hoeveelheidpunten = rit.lijn.len();
+                for i in 1..hoeveelheidpunten {
+                    let fysiek_punt_a = &rit.lijn[i - 1];
+                    let fysiek_punt_b = &rit.lijn[i];
 
-        d3.draw_line_3D(
-            Vector3 {
-                x: 2.0,
-                y: 0.0,
-                z: 0.0
-            },
-            Vector3 {
-                x: 2.0,
-                y: 10.0, 
-                z: 0.0
-            },
-            Color::YELLOW
-        );
+                    let punta = Vector3 {
+                        x: fysiek_punt_a.lng,
+                        y: fysiek_punt_a.lat,
+                        z: fysiek_punt_a.hoogte,
+                    };
+
+                    let puntb = Vector3 {
+                        x: fysiek_punt_b.lng,
+                        y: fysiek_punt_b.lat,
+                        z: fysiek_punt_b.hoogte,
+                    };
+
+                    d3.draw_line_3D(punta, puntb, Color::YELLOW);
+                }
+            }
+        }
+
+        d.draw_fps(10, 10);
     }
 
     Ok(())
